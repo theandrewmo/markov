5192020de296ce82bf7a80fc459956d927a9dcc1
Andrew Mo <106348010+theandrewmo@users.noreply.github.com>
2023-06-28 16:45:57 -0600

    markov starter code and package.json file

---
+++ markov.js
@@ -0,0 +1,37 @@
+/** Textual markov chain generator */
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    this.words = words.filter(c => c !== "");
+    this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    // TODO
+    let obj = {}
+    for (let word of this.words) {
+      if (!obj[word]) {
+        obj[word] = word
+      }
+    }
+    return 'made a chain'
+  }
+
+
+  /** return random text from chains */
+
+  makeText(numWords = 100) {
+    // TODO
+  }
+}
+
+let mm = new MarkovMachine('the cat in the hat')
